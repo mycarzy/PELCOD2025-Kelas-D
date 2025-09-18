f2d5fc26e35df777edf4899c80df342523531e1d
Haris Wahyu Ardianyah <hariswahyu777@gmail.com>
2025-09-18 13:19:20 +0700

    250441100085_HarisWahyuArdiansyah_Tugas1

---
+++ 250441100085_HarisWahyuArdiansyah/Tugas1.py
@@ -0,0 +1,29 @@
+# tugas ke-1
+print("--------tugas ke-1--------")
+nama = "Haris Wahyu Ardiansyah"
+umur = 18
+tinggi = 173.6
+angka_favorit = 13
+
+print("Nama saya :",nama,"\numur saya :",umur,"\ntinggi saya :",tinggi,"\nangka favorit saya :",angka_favorit)
+
+# tugas ke-2
+print("\n--------tugas ke-2--------")
+
+pensil = 2000
+buku = 5000
+
+hasil1 = 4 * pensil
+hasil2 = 3 * buku
+total = hasil1 + hasil2
+
+print("total harga  4 buah pensil dan  3 buah buku adalah :",total)
+
+
+# tugas ke-3
+print("\n--------tugas ke-3--------")
+
+if angka_favorit % 2 == 0:
+    print("angka genap")
+else:
+    print("angka ganjil")
